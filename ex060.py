a9ca89bb27d537302ed800ec736a9cf16e0ee848
seonwoo yoo <yoosunu010@naver.com>
2020-12-14 21:02:27 +0900

    Added file

---
+++ ex060.py
@@ -0,0 +1,3 @@
+nums = [1, 2, 3, 4, 5]
+average = sum(nums) / len(nums) # sum(nums)=> list인 nums의 원소 값의 합계, len(nums)=> list인 nums의 원소 값의 개수.
+print(average) # 원소 값의 합 = 15, 원소 값의 개수 = 5 이므로 3.0 이 출력 됨. 
